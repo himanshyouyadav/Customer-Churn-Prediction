b09141d64d52694bfc6d8e0d75c1b3e97d35dc36
Himanshu Yadav <himanshyouyadav@gmail.com>
2023-07-15 13:54:00 +0530

    Updated Requirments

--- app.py
+++ app.py
@@ -9,12 +9,6 @@ app = Flask('__name__')
 
 df_1=pd.read_csv("load_data.csv")
 df_1.drop('Unnamed: 0',axis=1,inplace=True)
-print(df_1.shape)
-
-
-data = [[0,29.85, 29.85, "Female", "Yes", "No", "No","No phone service","DSL","No","Yes","No","No","No","No",
-         "Month-to-month","Yes","Electronic check",1]]
-
 
 @app.route('/')
 def load_app():
@@ -61,22 +55,13 @@ def predict():
     #drop column customerID and tenure
     df_2.drop(columns= ['tenure'], axis=1, inplace=True)
 
-    print(df_2.columns.values,df_2.shape)
-
     dummy_df = pd.get_dummies(df_2)
 
     model = pickle.load(open("model.sav", "rb"))
     single = model.predict(dummy_df.tail(1))
 
     result = [single[0],model.predict_proba(dummy_df.tail(1))[:,1][0],model.predict_proba(dummy_df.tail(1))[:,0][0]]
-    # if single == 1:
-    #     probablity = model.predict_proba(dummy_df.tail(1))[:,1]
-    #     # result = "Customer is likely to churn! with a confidence of {0}".format(100*probablity[0])
-    #     print(result)
-    # else:
-    #     probablity = model.predict_proba(dummy_df.tail(1))[:,0]
-    #     # result = "Customer not likely to churn! with a confidence of {0}".format(100*probablity[0])
-    #     print(result)
+
     return render_template('home.html',result=result)    
 
 app.run(debug=False)
